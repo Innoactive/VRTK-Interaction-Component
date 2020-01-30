5f7c92a198aa9ad65c119f29e23149ef74e49f70
Maximilian Hotter <Maxsiom@users.noreply.github.com>
2020-01-30 11:16:19 +0100

    Bugfixes for teleporter and backwards compatibility.
    - chg: Put SnapZoneSettings.cs into Editor folder because it imports UnityEditor.
    - chg: Readded the Hub-SDK teleporter
    - chg: Added new script to controller
    - chg: Adjusted button layout to match Hub SDK.
    - chg: Added old meta files
    - chg: Updated version file to v1.0.1

---
+++ Source/Controller/Scripts/DisableComponentWhenTouching.cs
@@ -0,0 +1,80 @@
+using UnityEngine;
+using VRTK;
+
+namespace Innoactive.Hub.Interaction
+{
+    /// <summary>
+    /// Script that disables a set of components when the corresponding VRTK_InteractTouch touches an object.
+    /// </summary>
+    public class DisableComponentWhenTouching : MonoBehaviour
+    {
+        private static readonly Common.Logging.ILog logger = Innoactive.Hub.Logging.LogManager.GetLogger<DisableComponentWhenTouching>();
+
+        [SerializeField]
+        [Tooltip("When an object is touched, objects in toEnable are disabled, unless this field is true, in which case they are enabled")]
+        private bool setWhenTouching = false;
+
+        [SerializeField]
+        [Tooltip("Scripts to disable while the object is touched")]
+        private MonoBehaviour[] toDisable = null;
+
+        [SerializeField]
+        [Tooltip("InteractTouch to observe - if null, a VRTK_InteractTouch will be searched on this game object and its parents")]
+        private VRTK_InteractTouch interactHandler = null;
+
+        private void Reset()
+        {
+            GrabInteractHandler();
+        }
+
+        private void OnEnable()
+        {
+            GrabInteractHandler();
+
+            if (interactHandler != null)
+            {
+                interactHandler.ControllerTouchInteractableObject += HandleControllerTouchInteractableObject;
+                interactHandler.ControllerUntouchInteractableObject += HandleControllerUntouchInteractableObject;
+            }
+            else
+            {
+                logger.Error("No VRTK_InteractTouch found");
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (interactHandler != null)
+            {
+                interactHandler.ControllerTouchInteractableObject -= HandleControllerTouchInteractableObject;
+                interactHandler.ControllerUntouchInteractableObject -= HandleControllerUntouchInteractableObject;
+            }
+        }
+
+        private void GrabInteractHandler()
+        {
+            if (interactHandler == null)
+            {
+                interactHandler = GetComponentInParent<VRTK_InteractTouch>();
+            }
+        }
+
+        private void HandleControllerTouchInteractableObject(object sender, ObjectInteractEventArgs e)
+        {
+            SetAllActive(setWhenTouching);
+        }
+
+        private void HandleControllerUntouchInteractableObject(object sender, ObjectInteractEventArgs e)
+        {
+            SetAllActive(!setWhenTouching);
+        }
+
+        private void SetAllActive(bool activate = true)
+        {
+            foreach (MonoBehaviour component in toDisable)
+            {
+                component.enabled = activate;
+            }
+        }
+    }
+}
