4db598538ea0abf9949e33a14ee3b5a37fa2e7f6
Tom Wim <samo.miw@gmail.com>
2020-02-18 15:54:38 +0100

    Add Trainee Manager

---
+++ Source/Properties/TraineeManager.cs
@@ -0,0 +1,37 @@
+﻿using Innoactive.Hub.Training.SceneObjects.Properties;
+using Innoactive.Hub.Unity;
+using VRTK;
+
+namespace Innoactive.Hub.Training.Utils.VRTK
+{
+    /// <summary>
+    /// Makes sure that a <see cref="TraineeSceneObject"/> is applied to the current VRTK headset.
+    /// </summary>
+    public class TraineeManager : UnitySingleton<TraineeManager>
+    {
+        /// <summary>
+        /// Current trainee object.
+        /// </summary>
+        public TraineeSceneObject Trainee { get; protected set; }
+        
+        protected void OnEnable()
+        {
+            VRTK_SDKManager.instance.LoadedSetupChanged += OnVRTKLoaded;
+        }
+
+        private void OnDisable()
+        {
+            VRTK_SDKManager.instance.LoadedSetupChanged -= OnVRTKLoaded;
+        }
+
+        private void OnVRTKLoaded(VRTK_SDKManager sender, VRTK_SDKManager.LoadedSetupChangeEventArgs args)
+        {
+            if (Trainee != null)
+            {
+                Destroy(Trainee);
+            }
+            
+            Trainee = args.currentSetup.actualHeadset.AddComponent<TraineeSceneObject>();
+        }
+    }
+}
